e90c774b84d719081f5bb633530efc35a70a7dcf
Nicolas <nicolas.romito330@gmail.com>
2026-07-21 22:00:05 -0300

    fix

--- mobile/src/api/client.js
+++ mobile/src/api/client.js
@@ -6,7 +6,7 @@ export const BASE_URL = "https://cooking-app-fct9.onrender.com/api";
 // Backend local en la red WiFi
 // export const BASE_URL = "http://172.21.224.1:8000/api";
 //ip especial para android studio 
-export const BASE_URL = "http://10.0.2.2:8000/api";
+// export const BASE_URL = "http://10.0.2.2:8000/api";
 const client = axios.create({
   baseURL: BASE_URL,
   timeout: 15000, // un poco más alto: el free tier de Render puede tardar en "despertar"
